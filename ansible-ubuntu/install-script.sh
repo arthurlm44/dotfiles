22b7294cc877fd3a24857388956c9372f716bbb5
arthurlm44 <arthurlm44@gmail.com>
2018-04-26 20:19:07 -0700

    stuff

--- ansible-ubuntu/install-script.sh
+++ ansible-ubuntu/install-script.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 
 ## currently works for Ubuntu 16.04 -- maybe adapt for Arch in the future
-cat /etc/*-release | grep Xenial;
-IS_XENIAL=$?;
-
-if [ $IS_XENIAL -eq 0 ]
-then
   echo "Making sure old ansible is removed"
   sudo apt -y remove ansible
   echo "installing some python dependencies"
@@ -14,7 +9,4 @@ then
   sudo easy_install pip
   echo "installing ansible"
   sudo pip install ansible
-else
-  echo "Ubuntu Xenial (16.04) required"
-fi
 
