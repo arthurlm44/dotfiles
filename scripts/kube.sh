2f93cebf3e4925812dc3cf4bbbbe195ac9a94bad
arthurlm44 <arthurlm44@gmail.com>
2018-08-22 15:06:10 -0700

    add scripts

---
+++ scripts/kube.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+case $1 in
+  "up")
+    sudo swapoff -a
+    sudo sysctl net.bridge.bridge-nf-call-iptables=1
+    sudo kubeadm init
+    mkdir -p $HOME/.kube
+    sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+    sudo chown arthur. $HOME/.kube/config
+    kubectl taint nodes --all node-role.kubernetes.io/master-
+    kubectl apply -f "https://cloud.weave.works/k8s/net?k8s-version=$(kubectl version | base64 | tr -d '\n')"
+    ;;
+  "down")
+    rm -rf $HOME/.kube
+    sudo kubeadm reset
+    ;;
+esac
