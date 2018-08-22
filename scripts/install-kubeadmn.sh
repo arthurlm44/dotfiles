25d0a3ca52ad72392918216a3899c707e4cf6844
Arthur Le Maitre <arthurlm44@gmail.com>
2018-08-22 15:08:52 -0700

    also install docker

--- scripts/install-kubeadmn.sh
+++ scripts/install-kubeadmn.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-apt-get update && apt-get install -y apt-transport-https curl
+apt-get update && apt-get install -y apt-transport-https curl docker.io
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
 cat <<EOF >/etc/apt/sources.list.d/kubernetes.list
 deb http://apt.kubernetes.io/ kubernetes-xenial main
